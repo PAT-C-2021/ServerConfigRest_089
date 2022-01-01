752b2d0929e47ff5f8ff4a5f6e193b48314b2954
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-01-02 00:44:08 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140089_Prasetyo_Nur_Hidayat;
+
+namespace ServerConfigRest_20190140089_PrasetyoNurHidayat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+
+            try
+            {
+                hostObj = new ServiceHost(typeof(TI_UMY));
+                hostObj.Open();
+                Console.WriteLine("Server Ready!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+
+            catch (Exception e)
+            {
+                hostObj = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
